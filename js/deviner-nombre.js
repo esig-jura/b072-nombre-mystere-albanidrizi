6428f0984a43a88310ff4b25479f0f082069d425
alban.idrizi <alban.idrizi@divcom.ch>
2023-02-08 10:29:57 +0100

    ajout boucle nombre mystere

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,14 +1,35 @@
 /**
  * Jeu du nombre mystère
- * @author  Steve Fallet
+ * @author  Alban Idrizi
  * @version 1.0
- * @since   2022-08-30 (date de création)
+ * @since   2023-02-08 (date de création)
  */
 
-//Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
-(function main() {
-    'use strict';
+'use strict';
+/**
+ * Fonction qui retourne un nombre aléatoire compris entre min et max
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function tireNombre(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+console.log(tireNombre());
+const nbMystere = tireNombre(1,100);
+alert(nbMystere);
+let nbEssais = 0;
+let reponse = null;
+    do {
+        reponse = prompt('le nombre à deviner est compris entre 1 et 100.');
+        nbEssais= ++nbEssais;
+        if (reponse > nbMystere) {
+            alert('C\'est moins !');
+        } else if (reponse < nbMystere) {
+            alert('c\'est plus !');
+        }
+        else{
+            alert(`vous avez trouver après ${nbEssais} essais`);
+        }
+    } while (reponse !== nbMystere);
 
-   // Ton code de malade ici ...
-
-}()); //main IIFE
